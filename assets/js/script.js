1644de16ce3114322d90b1c48836bb31dd598d84
Henry-Ahn <henryahnn@gmail.com>
2025-07-26 13:29:21 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
